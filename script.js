9c9016f7c92e238bb588c6ec03794b25f7c56b02
Dhruw Vaviya <dhruwvaviya123@gmail.com>
2025-01-30 04:35:01 -0800

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function firstWord(s) {
   // your code here
-	let fWord = s.split(" ")[0];
-	return fWord;
-}
+	let arr = s.split(" ");
+	for(let i=0; i<arr.length; i++){
+		if(arr[i].length > 0){
+			return arr[i];
+		};
+	};
+};
 // Do not change the code below
 
 const s = prompt("Enter String:");
